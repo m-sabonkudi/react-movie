ff1b408903fd692366144d066c141ad47a305508
pyman <mlawalskudi@gmail.com>
2025-06-28 23:23:38 +0100

    hardcode back apikey (testing)

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 
 const BASE_URL = "https://api.themoviedb.org/3"
 
-const API_KEY = process.env.MOVIE_DB_API_KEY
+const API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMjhmOTM3OWRmNWFkYzNkZmY5ODVhYTcxYmQxM2U5ZCIsIm5iZiI6MTcwMzA4OTQ3Ni40MTY5OTk4LCJzdWIiOiI2NTgzMTU0NGVhN2IwZTUyNzcyNzA1M2YiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.0dJk34EnevHf6Za6urQ43AZwR_PAJVU2dGy69tjUetU"
 
 const API_OPTIONS = {
   method: 'GET',
